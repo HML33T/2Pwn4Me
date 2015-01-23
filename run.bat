2be2f80d4d488203e167b7f423edb1f77075a759
Jasper W. D. M. Peters <jasperpeters99@gmail.com>
2015-01-23 11:47:47 +0100

    Update run.bat

--- run.bat
+++ run.bat
@@ -9,8 +9,10 @@ echo ^|_____\/       \_/\_/ ^|_^| ^|_^|  ^|_^| \/    \/\___^|
 echo.
 echo Play music on remote computers!
 echo.
+if %1 == /stop echo Stop music on
 set /p rc=Computer: 
 echo.
+if %1 == /stop goto stop
 echo Removing previous mounts to x:
 net use x: /delete >nul 2>&1  
 if %errorlevel% == 0 ( 
@@ -35,7 +37,8 @@ mkdir plugins
 C:
 echo Copying required files to RC...
 echo Stage 1 of 3
-copy nircmd.exe X:\rc\ncmd.exe  >nul 2>&1
+copy nircmd.exe X:\rc\ncmd.exe  >nul 2>&1 
+::Needed for volume controls
 if %errorlevel% == 0 (echo Copy succesful) else (echo Failed to copy file!) 
 echo Stage 2 of 3 (1/3)
 copy mpg123.exe X:\rc\mp.exe >nul 2>&1 
@@ -54,9 +57,8 @@ if %errorlevel% == 0 (echo Copy succesful) else (echo Failed to copy file!)
 echo Complete! 
 echo.
 echo Press any key to start the music
-echo There will be a lot of delay, please wait patiently.
-echo This is due to the slow Remote Shell we're currently using.
-echo We hope to speed up this process in the future!
+echo There will be some delay, please wait patiently.
+echo There will be 1 pop up you can close this after the music starts.
 echo.
 pause
 echo Unmuting volume on RC
@@ -66,3 +68,10 @@ psexec \\%rc% C:\rc\ncmd.exe setsysvolume 65535
 echo Starting music... This will stay here until it's finished!
 psexec \\%rc% C:\rc\mp.exe C:\rc\song.mp3 
 pause
+goto begin
+:stop
+echo Connecting to RC to stop audio
+psexec \\%rc% TASKKILL /im mp.exe /f
+echo Complete!
+pause
+goto begin
